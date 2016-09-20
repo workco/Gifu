7aad6eeccedd925764cfca1a5d05291660572959
Vladimir Mitrovic <mitrovic@work.co>
2016-09-20 21:36:18 +0200

    Update for Swift 2.3
    Fixes the build error caused by not unwrapping an optional value.

--- Source/AnimatableImageView.swift
+++ Source/AnimatableImageView.swift
@@ -53,7 +53,7 @@ public class AnimatableImageView: UIImageView {
   /// - parameter imageName: The name of the GIF file. The method looks for the file in the app bundle.
   public func prepareForAnimation(imageNamed imageName: String) {
     currentLoopCount = 0
-    let imagePath = NSBundle.mainBundle().bundleURL.URLByAppendingPathComponent(imageName)
+    guard let imagePath = NSBundle.mainBundle().bundleURL.URLByAppendingPathComponent(imageName) else { return }
     prepareForAnimation <^> NSData(contentsOfURL: imagePath)
   }
 
